d0aa0497505ddb5df2648cf778e438d5f1eb0e19
Shiaba <>
2023-11-07 21:35:59 +0000

    Test

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shiaba-djangoblog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-shiaba-djangoblog-5k0jm9jbpo.us2.codeanyapp.com']
 
 
 # Application definition
